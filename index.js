c1ecbc82ad23935b2406ba16f8befc5f55916b2c
escher.marlie <escher.marlie@astralife.co.id>
2022-04-20 23:52:18 +0700

    Port number changed to 5000.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 3001;
+const port = 5000;
 const cors = require('cors');
 const moment = require('moment');
 
